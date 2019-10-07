6d74a5601fcbfd31a60728dcbfffd0f5b821c07e
Patrick Smith <patrick@burntcaramel.com>
2019-10-07 19:05:56 +1100

    Update live view

--- apps/collected_live_web/assets/js/app.js
+++ apps/collected_live_web/assets/js/app.js
@@ -11,9 +11,10 @@ import css from "../css/app.css"
 //
 import "phoenix_html";
 
+import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
-let liveSocket = new LiveSocket("/live")
+let liveSocket = new LiveSocket("/live", Socket)
 liveSocket.connect()
 
 import Vue from "vue";
