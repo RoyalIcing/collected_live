cc335c7382b55f1d2d8439ba3ba11cc344c9bf1e
Patrick Smith <patrick@burntcaramel.com>
2019-12-02 23:53:26 +1100

    Attempt to use musl with rust

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,8 +1,1 @@
-pwd
-ls /
-git clone https://github.com/llvm/llvm-project.git
-cd llvm-project
-mkdir build
-cd build
-cmake -DLLVM_ENABLE_PROJECTS=clang -G "Unix Makefiles" ../llvm
-make
+export CC_x86_64-unknown-linux-musl=musl-gcc
